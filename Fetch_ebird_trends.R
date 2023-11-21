b23575e92b81aa19ebb632bc774f5c8ea47d19da
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2023-11-21 13:53:55 -0500

    initial commit

---
+++ Fetch_ebird_trends.R
@@ -0,0 +1,121 @@
+#Fetch_ebird_trends
+
+library(ebirdst)
+library(tidyverse)
+library(bbsBayes2)
+library(terra)
+library(sf)
+
+map_single_program <- TRUE
+
+species <- "Wood Thrush"
+
+species <- "Louisiana Waterthrush"
+
+species <- "Eastern Bluebird"
+
+species <- "Western/Eastern Cattle Egret"
+
+path <- ebirdst_download_trends(species)
+
+trends <- load_trends(species,
+                      fold_estimates = TRUE)
+
+trends_sf <- st_as_sf(trends, 
+                      coords = c("longitude", "latitude"), 
+                      crs = 4326)
+
+latlong <- bbsBayes2::load_map("latlong") %>% 
+  st_transform(.,st_crs(trends_sf))
+
+
+trends_latlong_join <- trends_sf %>% 
+  st_join(.,latlong,
+          left = FALSE)
+
+trends_latlong <- trends_latlong_join %>% 
+  st_drop_geometry() %>% 
+  group_by(strata_name) %>%
+  summarise(abd_ppy_median = median(abd_ppy, na.rm = TRUE),
+            abd_ppy_lower = quantile(abd_ppy, 0.10, na.rm = TRUE),
+            abd_ppy_upper = quantile(abd_ppy, 0.90, na.rm = TRUE),
+            .groups = "drop") %>% 
+  arrange(abd_ppy_median)
+
+
+map_trends_latlong <- latlong %>% 
+  inner_join(.,trends_latlong,
+             by = "strata_name") %>% 
+  mutate(trend = abd_ppy_median,
+         survey = "eBird")
+if(map_single_program){
+map_ebird <- ggplot(map_trends_latlong) +
+  geom_sf(aes(fill = abd_ppy_median)) +
+  colorspace::scale_fill_continuous_diverging(rev = TRUE,
+                                              palette = "Blue-Red 2")+
+  guides(fill = guide_colorbar(title.position = "top", barwidth = 15)) +
+  labs(title = paste(species,"degree latlong breeding trends 2012-2022"),
+       fill = "Relative abundance trend [% change / year]") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+map_ebird
+}
+
+if(species == "Western/Eastern Cattle Egret"){
+  species <- "Cattle Egret"
+}
+# load BBS results --------------------------------------------------------
+aou <- as.integer(search_species(species)["aou"])
+fit <- readRDS(paste0("output/fit_",aou,".rds"))
+
+indices <- generate_indices(fit,hpdi = TRUE)
+ saveRDS(indices,
+         file = paste0("output/inds_",aou,".rds"))
+
+ indices <- readRDS(paste0("output/inds_",aou,".rds"))
+ traj <- plot_indices(indices,
+                      add_observed_means = TRUE)
+
+ trends <- generate_trends(indices,
+                          slope = FALSE,hpdi = TRUE)
+
+map_trends_bbs <- latlong %>% 
+  inner_join(.,trends$trends,
+             by = c("strata_name" = "region")) %>% 
+  mutate(survey = "BBS")
+# 
+if(map_single_program){
+  
+map_bbs <- ggplot(map_trends_bbs) +
+  geom_sf(aes(fill = trend)) +
+  colorspace::scale_fill_continuous_diverging(rev = TRUE,
+                                              palette = "Blue-Red 2")+
+  guides(fill = guide_colorbar(title.position = "top", barwidth = 15)) +
+  labs(title = paste(species,"degree latlong breeding trends 2012-2022"),
+       fill = "Relative abundance trend [% change / year]") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+map_bbs
+}
+
+
+map_trends_both <- bind_rows(map_trends_latlong,map_trends_bbs)
+
+map <- ggplot(map_trends_both) +
+  geom_sf(aes(fill = trend)) +
+  colorspace::scale_fill_continuous_diverging(rev = TRUE,
+                                              palette = "Blue-Red 2")+
+  guides(fill = guide_colorbar(title.position = "top", barwidth = 15)) +
+  labs(title = paste(species,"degree latlong breeding trends 2012-2022"),
+       fill = "Relative abundance trend [% change / year]") +
+  theme_bw() +
+  theme(legend.position = "bottom")+
+  facet_wrap(vars(survey))
+
+pdf(file = paste0("trend_comparison_",species,".pdf"),
+    width = 11,
+    height = 8.5)
+map
+dev.off()
---
+++ bbs_script.R
@@ -0,0 +1,73 @@
+## testing bbsBayes2 parallel in HRE env
+
+shhh <- suppressPackageStartupMessages # so I don't get a bunch of start up messages in the output file, a tip I encountered while searching through StackOverflow...
+shhh(library(bbsBayes2))
+shhh(library(tidyverse))
+
+setwd("C:/github/BBS_ebird_compare")
+
+
+sp_list <- readRDS("species_list.rds") %>%
+  filter(model == TRUE)
+
+
+
+# build cluster -----------------------------------------------------------
+
+   #for(i in 1:4){
+
+
+    sp <- "Cattle Egret"
+    
+i = which(sp_list[,"english"] == sp)
+aou <- as.integer(sp_list[i,"aou"])
+
+
+# identifying first years for selected species ----------------------------
+    fy <- 2012
+    # if(aou %in% c(4661,4660)){ #Alder and Willow Flycatcher
+    #   fy <- 1978 #5 years after the split
+    # }
+    # if(aou %in% c(10,11,22860)){ # Clark's and Western Grebe and EUCD
+    #   fy <- 1990 #5 years after the split and first year EUCD observed on > 3 BBS routes
+    # }
+    # if(aou == 6121){ # CAve Swallow
+    #   fy = 1985
+    # }
+
+
+
+   strat <- "latlong"
+
+   s <- stratify(by = strat,
+              species = sp,
+              quiet = TRUE) %>%
+  prepare_data(min_n_routes = 1,
+               min_max_route_years = 2,
+               quiet = TRUE,
+               min_year = fy)
+
+   ## bbsBayes2 models do not currently work unless n_strata > 1
+   if(nrow(s$meta_strata) == 1){stop(paste("Only 1 stratum for",sp,"skipping to next species"))}
+
+   if(nrow(s$meta_strata) > 2){ #spatial models are irrelevant with < 3 strata
+  bbs_dat <- prepare_spatial(s,
+                  strata_map = load_map(strat)) %>%
+  prepare_model(.,
+                model = "first_diff",
+                model_variant = "spatial")
+
+   }else{
+     bbs_dat <- prepare_model(s,
+                     model = "first_diff",
+                     model_variant = "hier")
+   }
+
+fit <- run_model(model_data = bbs_dat,
+                 refresh = 200,
+                 #iter_warmup = 300,
+                 #iter_sampling = 100,
+                 output_dir = "output",
+                 output_basename = paste0("fit_",aou))
+
+
